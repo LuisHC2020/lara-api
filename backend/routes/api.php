4ba2702688c02de9b514924bc7ed865efd5134ed
LuisHC2020 <67881149+LuisHC2020@users.noreply.github.com>
2023-06-15 05:54:09 -0500

    api implementado

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
